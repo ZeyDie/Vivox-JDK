3b7f9f82017a8a4ad9cbc05e743b505f256ab2da
Zalov-DV <zeydie.dev@gmail.com>
2024-04-08 17:09:03 +0500

    Remove unusable tests

--- src/test/java/client/VivoxTest.java
+++ src/test/java/client/VivoxTest.java
@@ -1,31 +1,39 @@
-package netty;
+package client;
 
 import com.zeydie.vivox.client.VivoxClient;
 import com.zeydie.vivox.common.services.netty.packets.RequestPackets;
 import com.zeydie.vivox.common.services.netty.utils.ByteBufUtil;
 import com.zeydie.vivox.server.VivoxServer;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@Log4j2
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class NettyTest {
+public class VivoxTest {
     private static final @NotNull VivoxClient vivoxClient = new VivoxClient();
     private static final @NotNull VivoxServer vivoxServer = new VivoxServer();
 
     @Test
     @Order(0)
-    public void init() {
+    public void pre() {
         vivoxServer.pre();
-        vivoxServer.init();
-        vivoxServer.post();
-
         vivoxClient.pre();
+    }
+
+    @Test
+    @Order(1)
+    public void init() {
+        vivoxServer.init();
         vivoxClient.init();
+    }
+
+    @Test
+    @Order(2)
+    public void post() {
+        vivoxServer.post();
         vivoxClient.post();
     }
 
@@ -47,7 +55,7 @@ public class NettyTest {
         val byteBuf = RequestPackets.REQUEST_JOIN_CHANNEL.createByteBufPacket();
 
         ByteBufUtil.writeString(byteBuf, VivoxClient.getUsername());
-        ByteBufUtil.writeString(byteBuf, "test_channel");
+        ByteBufUtil.writeString(byteBuf, VivoxClient.getChannel());
 
         VivoxClient.getNettyClientAPI().sendPacketToServer(byteBuf);
     }
